f8575cbae36d18abb4d140af8f771c4f71d496d4
Jors Academy <abalci16@posta.pau.edu.tr>
2026-08-29 22:18:12 +0300

    feat: export shortest-path decision-focused utilities

--- src/dfl_spo/__init__.py
+++ src/dfl_spo/__init__.py
@@ -5,18 +5,34 @@ from dfl_spo.evaluation import decision_regret, evaluate_model, normalized_regre
 from dfl_spo.losses import mse_loss, spo_plus_loss
 from dfl_spo.models import LinearCostPredictor
 from dfl_spo.oracle import objective_value, select_k
+from dfl_spo.shortest_path import (
+    GridGraph,
+    evaluate_shortest_path_model,
+    generate_shortest_path_data,
+    path_objective,
+    shortest_path_decision,
+    shortest_path_spo_plus_loss,
+    train_shortest_path_model,
+)
 from dfl_spo.training import train_model
 
 __all__ = [
     "DatasetSplit",
+    "GridGraph",
     "LinearCostPredictor",
     "decision_regret",
     "evaluate_model",
+    "evaluate_shortest_path_model",
     "generate_contextual_costs",
+    "generate_shortest_path_data",
     "mse_loss",
     "normalized_regret",
     "objective_value",
+    "path_objective",
     "select_k",
+    "shortest_path_decision",
+    "shortest_path_spo_plus_loss",
     "spo_plus_loss",
     "train_model",
+    "train_shortest_path_model",
 ]
